26fe1d20eeaf8da7a2e828503d57a2d953f56f4b
mv$ <modathirhussain12@gmail.com>
2025-09-19 21:54:41 +0100

    Update mkz-tool.bat

--- mkz-tool.bat
+++ mkz-tool.bat
@@ -1,6 +1,8 @@
 @echo off
 title mv4 made this one
 mode con: cols=120 lines=39
+echo WARNING!: make sure you have latest python version. press enter to continue
+pause
 python get-pip.py
 cls
 pip install -r code\requirements.txt
